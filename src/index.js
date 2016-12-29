1aff2c21a5ac9446cf360aeef1ad20876dbbb640
Simon JAILLET <sjaillet@gmail.com>
2016-12-29 23:24:35 +0100

    Remove Pagination class.

--- src/index.js
+++ src/index.js
@@ -2,12 +2,10 @@ var Schema = require('./schema');
 var Query = require('./query');
 var Json = require('./json');
 var Payload = require('./payload');
-var Paginate = require('./paginate');
 
 module.exports = {
   Schema: Schema,
   Query: Query,
   Json: Json,
-  Payload: Payload,
-  Paginate: Paginate
+  Payload: Payload
 };
